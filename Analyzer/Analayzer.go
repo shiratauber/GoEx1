e37c54f10929aaffc06e1bdb44b017b7360ccd39
Shira Tauber <shiratauber9@gmail.com>
2022-05-08 18:17:06 +0300

    start of EX3

---
+++ Analyzer/Analayzer.go
@@ -0,0 +1,19 @@
+package Analyzer
+
+import "os"
+
+type Analyzer struct {
+	outputFile  string
+	file        *os.File
+	current     string
+	CallCounter int
+	funcName    string
+}
+
+func New(path string) Analyzer {
+	return Analyzer{}
+}
+
+func call() {
+
+}
